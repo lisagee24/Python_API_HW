a6addae6e215f7f5c40d69ffc6c6a02f3e12ea2a
lisagee24 <47922534+lisagee24@users.noreply.github.com>
2019-04-07 21:08:01 -0400

    Add files via upload

---
+++ WeatherPy_HW/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+api_key = "8ac809a10ec5daeea2a37b05e525ddde"
+
+#http://api.openweathermap.org/data/2.5/weather?appid=53aa62c2dff88b5b2543ea80cc6837de&units=IMPERIAL&q="
+
+
